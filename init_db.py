6c2654ea5d3cea94715c3c06b0b94896a81361df
Ralph Ulysse <ralphulysse@Ralphs-MacBook-Air.local>
2025-06-22 21:40:04 -0400

    fix

--- init_db.py
+++ init_db.py
@@ -1,6 +1,7 @@
 # init_db.py
+
 from models import Base, engine
 
 if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
-    print("✅ Database tables created.")
+    print("✅ All tables created in the database.")
